e7d19cef10744671a72aabbb0421c7b2340f8cd2
MariusMM2 <dmunteanu91@gmail.com>
2021-01-05 15:50:36 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppContent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppContent from './components/AppContent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppContent
   }
 }
 </script>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppContent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
